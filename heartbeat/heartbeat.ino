c35bd71b6ce258385fca49466fae57d12e460e94
Raiyan <raiyan.jahangir@gmail.com>
2021-06-20 16:51:57 +0600

    first commit

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,103 @@
+#include<TimerOne.h>
+#include "pitches.h"
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+#define ledPin 7
+
+String my_name;
+
+
+#define USE_ARDUINO_INTERRUPTS true
+// Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>
+// Includes the PulseSensorPlayground Library.
+// Variables
+const int PulseWire = 0;
+
+// PulseSensor PURPLE WIRE connected to ANALOG PIN D
+const int LED13 = 13;
+//The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;
+//determine which Signal to "count as a beat" and which to ignore.
+//(/ Use the "Getting Started Project" to fine-tune Threshold Value beyond default setting.
+// Otherwise leave the default "550" value.
+PulseSensorPlayground pulseSensor; // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+void setup (){
+
+  pinMode(ledPin,OUTPUT);
+  //pinMode(buzzer_Pin,OUTPUT);
+//85%-> 96 (normal) 
+//70%-> 48 (low)
+//77%->63  (normal)
+  
+Serial.begin(9600) ;
+// For Serial Monitor
+// Configure the PulseSensor object, by assigning our variables to it.
+pulseSensor. analogInput (PulseWire) ;
+
+pulseSensor. blinkOnPulse (LED13) ;
+pulseSensor . setThreshold (Threshold) ;
+// Double-check the "pulseSensor" object was created and "began" seeing a signal.
+if (pulseSensor.begin () ) {
+Serial.println("We created a pulseSensor Object !"); //This prints one time at Arduino power-up, or on Arduino reset.
+}
+}
+
+
+
+void loop () {
+  int noteDuration;
+  Serial.print("Enter your Name:");
+  //analogWrite(ledPin,0);
+  while(Serial.available()==0){}
+        
+        my_name = Serial.readStringUntil('\n');
+ 
+        Serial.println("Nice to meet you, " + my_name + "!");
+   
+int myBPM = pulseSensor.getBeatsPerMinute();
+
+
+if (pulseSensor.sawStartOfBeat() ) {
+Serial.println ("A HeartBeat Happened ! ");
+Serial.print("BPM: ");
+Serial.println (myBPM);
+if( myBPM < 60){
+  Serial.println(my_name+", Your Heart Rate is Low and you are suffering from BRADYCARDIA");
+  analogWrite(ledPin,255);
+  int noteDuration = 2000;
+    tone(6, melody[0], noteDuration);
+    //int pauseBetweenNotes = noteDuration * 1.30;
+    //delay(pauseBetweenNotes);
+    //tone(6, 0, noteDuration);
+}
+else if( myBPM > 100){
+  Serial.println(my_name+", Your Heart Rate is High and you are suffering from TACHYCARDIA");
+  analogWrite(ledPin,255);
+   noteDuration = 2000;
+    tone(6, melody[0], noteDuration);
+    //int pauseBetweenNotes = noteDuration * 1.30;
+    //delay(pauseBetweenNotes);
+    //tone(6, 0, noteDuration);
+}
+else{
+  Serial.println(my_name+", Your Heart rate is normal");
+  analogWrite(ledPin,0);
+}
+}
+//tone(6, 0, noteDuration);
+// Print the value inside of myBEK.
+Serial.println();
+Serial.println();
+delay (20);
+  
+// considered best practice in a simple sketch.
+}
